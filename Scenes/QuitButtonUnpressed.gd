1bc632417dfad84b2432b74866222ad6974857b3
lisap1 <124928251+lisap1@users.noreply.github.com>
2024-05-20 22:10:46 +1200

    individual item assets added, item node framework

--- Scenes/QuitButtonUnpressed.gd
+++ Scenes/QuitButtonUnpressed.gd
@@ -1,27 +1,27 @@
 extends Sprite2D
 
 # setting sprites for items
-var apple = "res://Final Assets/play_button_pressed.png"
-var orange = "res://Final Assets/play_button_unpressed.png"
-const NUM_ITEMS = 2
-# associating items with correct bin
-var dict = {
-	apple : "green",
-	orange : "blue"
-}
-# list of items to generate random item
-var list = [apple, orange]
-
-func _ready():
-	set_process_input(true)
-	
-# selecting random item from the list and setting texture as the sprite associated
-func _on_timer_timeout():
-	var char_tex = list[randi() % NUM_ITEMS]
-	print(dict[char_tex])
-	var text = load(char_tex)
-	texture = text
-	# checking that the item variables can be found in the dictionary
-	if dict[char_tex] == "green":
-		print('yippee')
-		
+#var apple = "res://Final Assets/play_button_pressed.png"
+#var orange = "res://Final Assets/play_button_unpressed.png"
+#const NUM_ITEMS = 2
+## associating items with correct bin
+#var dict = {
+	#apple : "green",
+	#orange : "blue"
+#}
+## list of items to generate random item
+#var list = [apple, orange]
+#
+#func _ready():
+	#set_process_input(true)
+	#
+## selecting random item from the list and setting texture as the sprite associated
+#func _on_timer_timeout():
+	#var char_tex = list[randi() % NUM_ITEMS]
+	#print(dict[char_tex])
+	#var text = load(char_tex)
+	#texture = text
+	## checking that the item variables can be found in the dictionary
+	#if dict[char_tex] == "green":
+		#print('yippee')
+		#
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 const front_order = -1
 var exited = false
+var item_scene_resource = preload("res://Scenes/item.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var item_scene = item_scene_resource.instantiate()
+	add_child(item_scene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
